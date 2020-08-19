5285f773811ba03594bb64cfe319afd45339ebb2
salong01 <salong01@estudiantes.unileon.es>
2020-08-19 21:19:33 +0200

    form

--- docker-testing/src/app/app.module.ts
+++ docker-testing/src/app/app.module.ts
@@ -14,12 +14,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
--- docker-testing/src/app/home/home.component.ts
+++ docker-testing/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { NgForm } from '@angular/forms';
 
 interface OS{
   value: String
@@ -13,12 +14,24 @@ interface OS{
 export class HomeComponent{
   
   startDate = new Date(1990, 0, 1);
-  socialNet: String;
+  name: String;
+  lastName: String;
+  date: Date;
   email:String;
+  community: String;
+  city: String;
+  postalCode: String;
+  socialNet: String;
+  opSys: String;
+  
   os: OS[] = [
     {value: 'android', viewValue: 'Android'},
     {value: 'ios', viewValue: 'IOS'},
     {value: 'windows', viewValue: 'Windows mobile'},
   ]
+
+  onSubmit(form: NgForm) {
+    console.log('Your form data : ', form.value);
+}
   constructor(private dataService: DataService) { }
 }
